52570106f2daa654cc9355934281698f87fc84e4
DEVELOPER <DEVELOPER@host.docker.internal>
2020-09-10 13:49:50 +0700

    add first

---
+++ ProjectFlight/src/test/java/com/exercise/FlightProject/FlightProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exercise.FlightProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
