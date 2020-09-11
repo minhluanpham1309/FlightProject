8d1e1b6e74ac1531da32eb95c4e34ebc8cc72ad0
DEVELOPER <DEVELOPER@host.docker.internal>
2020-09-11 17:04:56 +0700

    push code date 11/09/2020

--- ProjectFlight/src/main/java/com/exercise/FlightProject/Controller/AdminController.java
+++ ProjectFlight/src/main/java/com/exercise/FlightProject/Controller/AdminController.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.exercise.FlightProject;
+package com.exercise.FlightProject.Controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
